e0124c762f448537130807056dd9fb485492e5e2
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-26 20:44:50 -0500

    parseInt saves lives

--- app.js
+++ app.js
@@ -36,19 +36,6 @@ NumberClassGroups.prototype.shuffleClass = function (){
 		} this.shuffledArray = shuffledArray;
 }
 
-//create groups from shuffledClass
-NumberClassGroups.prototype.generateGroups = function () {
-	for (var i = 0; i < this.numGroups; i++){
-		if (this.extras > 0){
-			this["group"+(i+1)] = this.shuffledArray.splice(0, this.baseGroupSize+1);
-			this.extras--;
-		}else{
-			this["group"+(i+1)] = this.shuffledArray.splice(0, this.baseGroupSize);
-		}
-	}
-}
-
-
 //Define the SizeClassGroups constructor
 var SizeClassGroups = function(groupSize, classNames) {
 	this.classNames = classNames,
@@ -59,24 +46,37 @@ SizeClassGroups.prototype = new NumberClassGroups();
 SizeClassGroups.prototype.constructor = SizeClassGroups;
 
 
-//Set 
+//Set number of group and extras
 SizeClassGroups.prototype.getGroupNumber = function () {
 	this.numGroups = Math.floor(this.classNames.length / this.baseGroupSize);
-	this.extras = this.classNames.length - (this.numGroups * this.baseGroupSize);
+	this.extras = this.classNames.length - this.numGroups * this.baseGroupSize;
 }
 
+NumberClassGroups.prototype.generateGroups = function () {
+	for (var i = 0; i < this.numGroups; i++){
+		if (this.extras > 0){
+			this["group"+(i+1)] = this.shuffledArray.splice(0, parseInt(this.baseGroupSize)+1);
+			this.extras--;
+		}else{
+			this["group"+(i+1)] = this.shuffledArray.splice(0, this.baseGroupSize);
+		}
+	}
+}
+
+//callback function for group and size buttons
 function select() {
 		$('.selected').removeClass('selected');
 		$(this).addClass('selected');
+		$(".listArea").empty();
 	}
 
 
 $(document).ready(function (){
 	$(".groupButton").click(select);
 	$(".sizeButton").click(select);
-
+	
+	//generating group objects and creating groups
 	$("#generate").click(function(){
-		$(".container").remove();
 		var selected = $(".selected")
 		var classGroups;
 		if ($(selected).is(".groupButton")){
@@ -87,17 +87,18 @@ $(document).ready(function (){
 			classGroups.getGroupNumber();
 		}
 		
-
 		classGroups.shuffleClass();
 		classGroups.generateGroups();
 
-		
+		//Appending group and team names to the list area		
 		var nameId = 0;
+		var groupNum;
 		for ( var i = 0; i < classGroups.numGroups; i++){
-			$(".listArea").append("<div class=container> <ul id ='group" + (i+1) + "'>Group "  + (i+1) + "</ul></div>");
-			for (var j = 0; j < classGroups["group" + (i+1)].length; j++){
+			groupNum = "group" + (i+1);
+			$(".listArea").append("<div class=container> <ul id ='" + groupNum + "'>Group "  + (i+1) + "</ul></div>");
+			for (var j = 0; j < classGroups[groupNum].length; j++){
 				nameId++;
-				$("<li id='name" + nameId + "'>"  + classGroups["group" + (i+1)][j] + "</li>").appendTo("#group"+(i+1)).hide();
+				$("<li id='name" + nameId + "'>"  + classGroups[groupNum][j] + "</li>").appendTo("#group"+(i+1)).hide();
 				$("#name"+nameId).delay(nameId * 500).slideDown("slow");
 			}
 		}
